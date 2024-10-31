249d550b5826a5fb7dbd5bc6b7d570bf5b2fc1cb
Jasur <ahmedovj5016@gmail.com>
2024-10-31 20:13:01 +0500

    add author_detail

--- app_books/views.py
+++ app_books/views.py
@@ -22,4 +22,30 @@ def author_list(request):
     else:
         return HttpResponse("Invalid request method", status=405)
 
+
 @api_view(['PUT', 'DELETE', 'PATCH'])
+def author_detail(request, pk):
+    try:
+        author = AuthorModel.objects.get(pk=pk)
+    except AuthorModel.DoesNotExist:
+        return HttpResponse("Author not found", status=404)
+    if request.method == 'GET':
+        serializer = AuthorModelSerializer(author)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = AuthorModelSerializer(author, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = {'success': True, 'message': 'Put Successfully', "data": serializer.data}
+        return Response(response, status=200)
+    elif request.method == 'DELETE':
+        author.delete()
+        return Response({'success': True, 'message': 'Delete Successfully'}, status=204)
+    elif request.method == 'PATCH':
+        serializer = AuthorModelSerializer(author, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = {'success': True, 'message': 'Patch Successfully', "data": serializer.data}
+        return Response(response, status=200)
+    else:
+        return HttpResponse("Invalid request method", status=405)
